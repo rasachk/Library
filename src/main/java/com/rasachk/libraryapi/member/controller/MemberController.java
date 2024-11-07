d09bd535e5d5f678ac35be2ed892ea4187d2ef2c
Rasa Chekeni <chekani.rasa@gmail.com>
2024-11-07 15:07:59 +0330

    Feat: V-1.1.0 Used Interface And Abstraction For User Service

--- src/main/java/com/rasachk/libraryapi/member/controller/MemberController.java
+++ src/main/java/com/rasachk/libraryapi/member/controller/MemberController.java
@@ -4,58 +4,46 @@ import com.rasachk.libraryapi.authentication.AuthenticationResponse;
 import com.rasachk.libraryapi.member.dto.MemberDto;
 import com.rasachk.libraryapi.member.service.MemberService;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api
 @RestController
 @RequestMapping("/api")
-@Api
+@RequiredArgsConstructor
 public class MemberController {
-    @Autowired
-    private MemberService memberService;
 
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
+    private final MemberService memberService;
 
     @PostMapping("/members/create")
     public ResponseEntity<AuthenticationResponse> createMember(@RequestBody MemberDto memberDto) {
-        return new ResponseEntity<AuthenticationResponse>(memberService.saveMember(memberDto), HttpStatus.OK);
+        return new ResponseEntity<>(memberService.saveMember(memberDto), HttpStatus.OK);
     }
-
-//    @PostMapping("/authenticate")
-//    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-//        return new ResponseEntity<AuthenticationResponse>(memberService.authenticate(request), HttpStatus.OK);
-//    }
-
-    @GetMapping("/members/list")
-    public ResponseEntity<List<MemberDto>> getAllMembers() {
-        return new ResponseEntity<List<MemberDto>>(memberService.findAllMembers(), HttpStatus.OK);
-    }
-
-    @GetMapping("/{username}")
-    public ResponseEntity<MemberDto> findOneMember(@PathVariable String username) {
-        return new ResponseEntity<MemberDto>(memberService.convertMemberToDto(memberService.findMember(username)), HttpStatus.OK);
-    }
-
 
     @PostMapping("/update")
     public ResponseEntity<MemberDto> updateMember(@RequestBody MemberDto memberDto) {
-        return new ResponseEntity<MemberDto>(memberService.update(memberDto), HttpStatus.OK);
+        return new ResponseEntity<>(memberService.update(memberDto), HttpStatus.OK);
     }
 
-    /*    @PatchMapping("/members/{username}")
-        public ResponseEntity<MemberDto> updateMember(@PathVariable String username, @RequestBody JsonPatch patch){
-            return ResponseEntity<MemberDto>(memberService.updateMember();)
-        }*/
     @DeleteMapping("/{username}")
-    public ResponseEntity<String> deleteMember(@PathVariable("username") String username) {
+    public ResponseEntity<Void> deleteMember(@PathVariable("username") String username) {
         memberService.deleteMember(username);
-        return new ResponseEntity<String>(HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping("/members/list")
+    public ResponseEntity<List<MemberDto>> getAllMembers() {
+        return new ResponseEntity<>(memberService.findAllMembers(), HttpStatus.OK);
+    }
+
+//    @GetMapping("/{username}")
+//    public ResponseEntity<MemberDto> findOneMember(@PathVariable String username) {
+//        return new ResponseEntity<>(memberService.findMember(username), HttpStatus.OK);
+//    }
+
+
 }
---
+++ src/main/java/com/rasachk/libraryapi/member/service/MemberServiceImpl.java
@@ -0,0 +1,134 @@
+package com.rasachk.libraryapi.member.service;
+
+import com.rasachk.libraryapi.authentication.AuthenticationResponse;
+import com.rasachk.libraryapi.exceptions.ResourceNotFoundException;
+import com.rasachk.libraryapi.member.dao.MemberRepository;
+import com.rasachk.libraryapi.member.dto.MemberDto;
+import com.rasachk.libraryapi.member.entity.Member;
+import com.rasachk.libraryapi.member.entity.Role;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+    private final ModelMapper modelMapper;
+
+/*    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = findMember(username);
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        member.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.toString()));
+        });
+
+        return new User(member.getUsername(), member.getPassword(), authorities);
+        //BUG??
+    }*/
+
+    public AuthenticationResponse saveMember(MemberDto memberDto) {
+        Member member = modelMapper.map(memberDto, Member.class);
+        member.setPassword(memberDto.getPassword());
+        member.setRole(Role.USER);
+        memberRepository.save(member);
+        return AuthenticationResponse.builder()
+                .username(memberDto.getUsername())
+                .role(member.getRole())
+                .build();
+    }
+
+//    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+//        authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(
+//                        request.getUsername(),
+//                        request.getPassword()
+//                )
+//        );
+//        Member member = findMember(request.getUsername());
+//        var jwtToken = jwtService.generateToken(member);
+//        return AuthenticationResponse.builder()
+//                .username(request.getUsername())
+//                .role(member.getRole())
+//                .token(jwtToken)
+//                .build();
+//    }
+
+    public MemberDto convertMemberToDto(Member member) {
+        return modelMapper.map(member, MemberDto.class);
+    }
+
+    public Member convertDtoToMember(MemberDto memberDto) {
+        return findMember(memberDto.getUsername());
+        //return modelMapper.map(memberDto,Member.class);
+    }
+
+    public List<MemberDto> findAllMembers() {
+        return memberRepository.findAllByAvailability(true)
+                .stream()
+                .map(Member -> modelMapper.map(Member, MemberDto.class))
+                .collect(Collectors.toList());
+    }
+
+    public Member findMember(String username) {
+        Member member = memberRepository.findMemberByUsernameAndAvailability(username, true);
+        if (member == null) {
+            throw new ResourceNotFoundException("Member", "username", username);
+        }
+        return member;
+    }
+
+
+    public MemberDto update(MemberDto memberDto) {
+        Member member = convertDtoToMember(memberDto);
+        member.setUsername(memberDto.getUsername());
+        member.setName(memberDto.getName());
+        member.setGender(memberDto.getGender());
+        member.setAge(memberDto.getAge());
+        memberRepository.save(member);
+        return memberDto;
+    }
+
+    public void deleteMember(String username) {
+        Member member = findMember(username);
+        member.setAvailability(false);
+        memberRepository.save(member);
+    }
+
+    public String encryptPassword(String password) {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            // of an input digest() return array of byte
+            byte[] messageDigest = md.digest(password.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
